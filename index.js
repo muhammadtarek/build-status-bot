5861533b53e6828666d7957eb5adae9ca1d6d1ea
Muhammad Tarek <muhammadtarek96@gmail.com>
2018-02-17 18:10:13 +0200

    Add source branch, requested by and pull request options

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const chalk = require('chalk');
 const { WebClient } = require('@slack/client');
 const commandLineArgs = require('command-line-args'); // Command args tokenizer
-const getUsage = require('command-line-usage'); // Command options help
 const markdownParser = require('./MarkdownParser');
 
 const optionDefinitions = [
@@ -9,59 +8,80 @@ const optionDefinitions = [
     name: 'package-name',
     alias: 'n',
     type: String,
-    defaultOption: true
+    defaultOption: true,
   },
   {
     name: 'message',
     alias: 'm',
     type: String,
-    defaultValue: undefined
+    defaultValue: undefined,
   },
   {
     name: 'server',
     alias: 's',
     type: String,
-    defaultValue: undefined
+    defaultValue: undefined,
   },
   {
-    name: 'build-id',
+    name: 'build',
     alias: 'b',
     type: String,
-    defaultValue: undefined
+    defaultValue: undefined,
   },
   {
     name: 'path',
     alias: 'p',
     type: String,
-    defaultValue: 'CHANGELOG.md'
+    defaultValue: 'CHANGELOG.md',
   },
   {
     name: 'channel',
     alias: 'c',
-    type: String
+    type: String,
   },
   {
     name: 'auth',
     alias: 'a',
-    type: String
+    type: String,
   },
   {
-    name: 'help',
-    alias: 'h',
-    type: Boolean,
-    lazyMultiple: false,
-    multiple: false
-  }
+    name: 'repo-url',
+    alias: 'u',
+    type: String,
+    defaultValue: undefined,
+  },
+  {
+    name: 'pull-request',
+    alias: 'r',
+    type: String,
+    defaultValue: undefined,
+  },
+  {
+    name: 'source-branch',
+    alias: 'g',
+    type: String,
+    defaultValue: undefined,
+  },
+  {
+    name: 'requested-for',
+    alias: 'f',
+    type: String,
+    defaultValue: 'VSTS CI',
+  },
 ];
 
 const userOptions = commandLineArgs(optionDefinitions); // User options
-const packageName = userOptions['package-name'];
-let message = userOptions['message'];
-const server = userOptions['server'];
-const buildId = userOptions['build-id'];
-const packageChangelogPath = userOptions['path'];
-const channelName = userOptions['channel'];
-const slackOAuth = userOptions['auth'];
+const packageName = userOptions['package-name'] || process.env.REPO;
+const message = userOptions.message;
+const server = userOptions.server;
+const buildId = userOptions.build || process.env.BUILD;
+const packageChangelogPath = userOptions.path;
+const channelName = userOptions.channel || process.env.CHANNEL;
+const slackOAuth = userOptions.auth;
+const repoUrl = userOptions['repo-url'] || process.env.REPO_URL;
+const pullRequest = userOptions['pr-number'] || process.env.PULL_REQUEST;
+const sourceBranch = userOptions['source-branch'] || process.env.SOURCE_BRANCH;
+const requestedFor = userOptions['requested-for'];
 const versionChangelog = markdownParser.getLastVersion(packageChangelogPath);
 const bulletPoints = versionChangelog.versionPoints.map(point => `• ${point}`);
 const styledPoints = `${bulletPoints.join('\n')}`;
@@ -73,48 +93,51 @@ const attachments = {
       fields: [
         {
           title: 'Repository',
-          value: `\`${packageName}\``,
-          short: true
+          value: `\`<${repoUrl}|${packageName}>\``,
+          short: true,
         },
         {
           title: 'Version',
-          value: `\`${versionChangelog.version}\``,
-          short: true
+          value: `\`<${repoUrl}?version=GTv${versionChangelog.version}|${versionChangelog.version}>\``,
+          short: true,
+        },
+        {
+          title: 'Pull Request',
+          value: `<${repoUrl}/pullrequest/${pullRequest}|${pullRequest}>`,
+          short: true,
+        },
+        {
+          title: 'Source Branch',
+          value: `<${repoUrl}/?version=GB${sourceBranch}|${sourceBranch}>`,
+          short: true,
+        },
+        {
+          title: 'Server',
+          value: `\`${server}\``,
+          short: true,
+        },
+        {
+          title: 'VSTS Build Definition',
+          value: `\`${buildId}\``,
+          short: true,
         },
         {
           title: 'Changelog',
-          value: styledPoints
-        }
-      ]
-    }
-  ]
+          value: styledPoints,
+        },
+      ],
+      footer: `Request by: ${requestedFor}`,
+    },
+  ],
 };
 
-// Adding server section
-if (server) {
-  attachments.attachments[0].fields.splice(2, 0, {
-    title: 'Server',
-    value: `\`${server}\``,
-    short: true
-  });
-}
-
-// Adding build id on VSTS
-if (buildId) {
-  attachments.attachments[0].fields.splice(3, 0, {
-    title: 'VSTS Build ID',
-    value: `\`${buildId}\``,
-    short: true
-  });
-}
-
-// Adding :tada: emoji before the message
-if (message) {
-  message = `:tada: ${message}`;
-}
+// Removing fields with undefined value or with empty strings
+const fieldsWithValue = attachments.attachments[0].fields
+  .filter(field => field.value.indexOf('undefined') === -1)
+  .filter(field => field.value !== '');
+attachments.attachments[0].fields = fieldsWithValue;
 
 // Getting access token from slack.com
-const authUrl = 'https://slack.com/oauth/authorize';
 const token = process.env.SLACK_OAUTH_ACCESS_TOKEN || slackOAuth;
 const client = new WebClient(token);
 const channel = `#${channelName}`;
@@ -122,7 +145,7 @@ const channel = `#${channelName}`;
 // See: https://api.slack.com/methods/chat.postMessage
 client.chat
   .postMessage(channel, message, attachments)
-  .then(res => {
+  .then(() => {
     console.log(chalk.green('Message posted successfully'));
   })
-  .catch(console.error);
+  .catch(err => console.log(chalk.red(err)));
